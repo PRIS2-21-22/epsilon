e556e5ca55a8e080db2b91c98401098e7a51fa68
lop427 <nicoop1998@gmail.com>
2021-03-24 19:20:01 +0100

    update

--- epsilon.py
+++ epsilon.py
@@ -1,21 +1,22 @@
 class epsilon:
+    
     value=[] #trapezoide o triangulo
 
     def __init__(self, m, a, b, d):
         #triangulo
-        if d==none
+        if (d==none)
             self.value=[a,b,d]
         else: #trapezoide
             self.value=[self,a,b,d]
 
     def suma(self,num):
-            if len(self.value)==3 and leng(num.value)==3:
+        if (len(self.value)==3 and leng(num.value)==3):
                 return [self.value[0] + num.value[0], self.value[1] + num.value[1], self.value[2] + num.value[2]]
-            else:
+          else:
                 return [self.value[0] + num.value[0], self.value[1] + num.value[1], self.value[2] + num.value[2], self.value[3] + num.value[3]]
 
     def resta(self, num):
-        if(len(num.value) == 3 and len(self.value) == 3):
+        if (len(num.value) == 3 and len(self.value) == 3):
             return self.suma(borroso(-num.value[0], -num.value[1], -num.value[2], d=None))
         else:
             return self.suma(borroso(-num.value[0],-num.value[1],-num.value[2],-num.value[3]))
